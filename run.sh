ba37fbc0667ce4600e4df2da75d6b86965d82de0
Adrien Folie <folie.adrien@gmail.com>
2015-02-16 14:57:14 +0100

    copy properly custom allow file

--- run.sh
+++ run.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 set -e
 
-target="/etc/nginx/nginx.conf"
+nginx_conf="/etc/nginx/nginx.conf"
+allow="/etc/nginx/allow.conf"
 
 if [ "$SSL" = "on" ]; then
-    mv nginx_ssl.conf $target
+    mv nginx_ssl.conf $nginx_conf
 else
-    mv nginx.conf $target
+    mv nginx.conf $nginx_conf
+fi
+
+allow_origin="/config/allow.conf"
+allow_target="/etc/nginx/allow.conf"
+
+# If user add a custom allow file
+if [ -f $allow_origin ]
+    mv $allow_origin $allow_target
 fi
 
 ep -v $target -- /usr/sbin/nginx
